708c3ce05e53c177f3f2096772d7506acbec32a3
augustkiid <erretamv@gmail.com>
2025-12-24 23:49:59 +0400

    dddd
    git add find_max.py
    git commit -m "Добавлена проверка на пустой список с выбросом ValueError"

--- find_max.py
+++ find_max.py
@@ -13,4 +13,13 @@ def find_max(numbers):
         if number > max_value:
             max_value = number
     return max_value
+def find_max(numbers):
+    if not numbers:
+        raise ValueError("List must not be empty")
+
+    max_value = numbers[0]
+    for number in numbers:
+        if number > max_value:
+            max_value = number
+    return max_value
 
