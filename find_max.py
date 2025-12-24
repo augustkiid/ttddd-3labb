ec8ef42c4f069a0c48578ab9fabc80079f27caca
augustkiid <erretamv@gmail.com>
2025-12-24 23:48:06 +0400

    Update find_max.py
    git add find_max.py
    git commit -m "Реализован поиск максимума для первых элементов списка"

--- find_max.py
+++ find_max.py
@@ -1,2 +1,9 @@
 def find_max(numbers):
     return 3
+def find_max(numbers):
+    max_value = numbers[0]
+    if numbers[1] > max_value:
+        max_value = numbers[1]
+    if len(numbers) > 2 and numbers[2] > max_value:
+        max_value = numbers[2]
+    return max_value
