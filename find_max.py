42916ccbe4c422d43b88c1460f94e345187eddec
augustkiid <erretamv@gmail.com>
2025-12-25 11:50:02 +0400

    Update find_max.py

--- find_max.py
+++ find_max.py
@@ -1,19 +1,4 @@
 def find_max(numbers):
-    return 3
-def find_max(numbers):
-    max_value = numbers[0]
-    if numbers[1] > max_value:
-        max_value = numbers[1]
-    if len(numbers) > 2 and numbers[2] > max_value:
-        max_value = numbers[2]
-    return max_value
-def find_max(numbers):
-    max_value = numbers[0]
-    for number in numbers:
-        if number > max_value:
-            max_value = number
-    return max_value
-def find_max(numbers):
     if not numbers:
         raise ValueError("List must not be empty")
 
@@ -21,5 +6,5 @@ def find_max(numbers):
     for number in numbers:
         if number > max_value:
             max_value = number
-    return max_value
 
+    return max_value
